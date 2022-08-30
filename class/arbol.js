7db4a255711fc6d8d8413b46ab3db2ba09e04d8e
Jair Santiago Leal Miranda <jair.leal@udea.edu.co>
2022-08-30 15:38:20 -0500

    comentarios del codigo

--- class/arbol.js
+++ class/arbol.js
@@ -1,25 +1,25 @@
 
-var Stack = /** @class */ (function () {
+var Stack = /** @class */ (function () { //clase pila 
     function Stack() {
         this.vector = new Array();
     }
-    Stack.prototype.push = function (a) {
+    Stack.prototype.push = function (a) { //metodo que agrega un elemento a la cima de la pila
         this.vector.push(a);
     };
-    Stack.prototype.pop = function () {
+    Stack.prototype.pop = function () { //metodo que elimina el elemento de la cima de la pila
         return this.vector.pop();
     };
-    Stack.prototype.empty = function () {
+    Stack.prototype.empty = function () { //metodo que verifica si la pila esta vacia
         return this.vector.length == 0;
     };
     return Stack;
 }());
-var nodoDoble = /** @class */ (function () {
+var nodoDoble = /** @class */ (function () { //clase nodo doble(cuatro espacios)
     function nodoDoble(valor) {
-        this.sw = 0;
-        this.valor = valor;
-        this.ligaIzq = null;
-        this.ligaDer = null;
+        this.sw = 0; //variable que indica si el valor del nodo es o no un subarbol
+        this.valor = valor; //valor del nodo
+        this.ligaIzq = null; //liga del nodo a la izquierda, en el se guarda el nodo padre
+        this.ligaDer = null; //liga del nodo a la derecha, en el se guarda el nodo hijo o nodo hermano
     }
     nodoDoble.prototype.getLigaIzq = function () {
         return this.ligaIzq;
@@ -47,16 +47,14 @@ var nodoDoble = /** @class */ (function () {
     };
     return nodoDoble;
 }());
-var arbol = /** @class */ (function () {
-    function arbol(arbolHilera) {
-        var longitud = arbolHilera.length;
-        if (longitud < 1)
-            return;
-        if (longitud == 3) {
+var arbol = /** @class */ (function () { //clase arbol
+    function arbol(arbolHilera) { //constructor de la clase arbol, que recibe un string con los datos del arbol a crear
+        var longitud = arbolHilera.length;  //longitud del string
+        if (longitud == 3) { //si la longitud del string es 3, el arbol solo tiene un nodo que es la raiz
             this.raiz = new nodoDoble(arbolHilera[1]);
             return;
         }
-        this.raiz = new nodoDoble(arbolHilera[1]);
+        this.raiz = new nodoDoble(arbolHilera[1]); //se crea la raiz del arbol
         var auxPadre, hoja, nodoAux;
         var pila = new Stack();
         auxPadre = this.raiz;
@@ -97,128 +95,129 @@ var arbol = /** @class */ (function () {
             }
         }
     }
-    arbol.prototype.finDeRecorrido = function (p) {
+    arbol.prototype.finDeRecorrido = function (p) { //metodo que verifica si un nodo salio del recorrido
         return p == null || p == undefined;
     };
 
-    arbol.prototype.imprimir = function () {
-        let string = [];
-        let padre = true;
-        string.push("(");
-        let p = this.raiz;
-        let pila = new Stack();
-        while (!this.finDeRecorrido(p)) {
-            if (p.sw == 0) {
-                string.push(p.valor);
-                string.push(padre?"(":this.finDeRecorrido(p.getLigaDer()) ? ")": ",");
-                padre = false;
-                p = p.getLigaDer();
-                while (!pila.empty() && p == null) {
+    arbol.prototype.imprimir = function () { //metodo que imprime el arbol
+        let string = []; //string que almacena el arbol
+        let padre = true; //variable que indica si el nodo actual es el padre de un subarbol
+        string.push("("); //se agrega un parentesis al inicio del arbol
+        let p = this.raiz; //se crea un puntero que apunta al nodo raiz
+        let pila = new Stack(); //pila donde se guardan los nodos con subarboles
+        while (!this.finDeRecorrido(p)) { //mientras el puntero no apunte a un nodo nulo
+            if (p.sw == 0) { //si el nodo no tiene subarbol
+                string.push(p.valor); //se agrega el valor del nodo al string
+                string.push(padre?"(":this.finDeRecorrido(p.getLigaDer()) ? ")": ","); //se agrega un abre parentesis, cierra parentesis o una coma segun sea el caso
+                padre = false; //se indica que el nodo actual no es el padre de un subarbol
+                p = p.getLigaDer(); //se mueve el puntero al nodo derecho
+                while (!pila.empty() && p == null) { //mientras la pila no este vacia y el puntero apunte a un nodo nulo
                     p = pila.pop();
-                    string.push(this.finDeRecorrido(p.getLigaDer()) ? ")": null);
+                    string.push(this.finDeRecorrido(p.getLigaDer()) ? ")": null); //se agrega un cierra parentesis o una coma segun sea el caso
                     p = p.getLigaDer();
                 }
             }
-            else {
-                pila.push(p);
-                p = p.valor;
-                padre = true;
+            else { //si el nodo tiene subarbol
+                pila.push(p); //se agrega el nodo actual a la pila
+                p = p.valor; //se mueve el puntero al subarbol
+                padre = true; //se indica que el nodo actual es el padre de un subarbol
             }
         }
-        string.push(")");
+        string.push(")"); //se agrega un cierra parentesis al final del arbol
         return string.join("");
     };
 
-    arbol.prototype.gradoRegistro = function (pa) {
-        if (pa.getSw()==0 && pa != this.raiz){
+    arbol.prototype.gradoRegistro = function (pa) { //metodo que calcula el grado de un nodo
+        if (pa.getSw()==0 && pa != this.raiz){ //si el nodo no tiene subarbol y no es la raiz
+            return 0; //se retorna 0
+        }
+        let p = (pa == this.raiz)? pa: pa.getValor(); //se crea un puntero que apunta al nodo actual o al subarbol del nodo actual, segun si el nodo actual es la raiz o no
+        if (this.finDeRecorrido(p)) { //si el puntero apunta a un nodo nulo, se retorna 0
             return 0;
         }
-        let p = (pa == this.raiz)? pa: pa.getValor();
-        if (this.finDeRecorrido(p)) {
-            return 0;
-        }
-        p = p.getLigaDer();
-        let i = 0;
-        while(!this.finDeRecorrido(p)) {
+        p = p.getLigaDer(); //se mueve el puntero al nodo derecho
+        let i = 0; //contador que cuenta el grado del nodo actual
+        while(!this.finDeRecorrido(p)) { //mientras el puntero no apunte a un nodo nulo, se suma 1 al contador
             i++;
             p = p.getLigaDer();
         }
-        return i
+        return i //se retorna el contador
     }
 
-    arbol.prototype.gradoMayor = function () {
-        let p = this.raiz;
-        let pila = new Stack();
-        let mayor = this.gradoRegistro(p);
-        let aux = mayor;
-        while (!this.finDeRecorrido(p)) {
-            if (p.sw == 0) {
+    arbol.prototype.gradoMayor = function () { //metodo que calcula el grado mayor del arbol
+        let p = this.raiz; //se crea un puntero que apunta al nodo raiz
+        let pila = new Stack(); //pila donde se guardan los nodos con subarboles
+        let mayor = this.gradoRegistro(p); //se calcula el grado del nodo raiz
+        let aux = mayor; //se guarda el grado del nodo raiz, en una variable auxiliar
+        while (!this.finDeRecorrido(p)) { //mientras el puntero no apunte a un nodo nulo
+            if (p.sw == 0) { //si el nodo no tiene subarbol, avanza al nodo derecho 
                 p = p.getLigaDer();
                 while (!pila.empty() && p == null) {
                     p = pila.pop();
                     p = p.getLigaDer();
                 }
             }
-            else {
-                pila.push(p);
+            else { //si el nodo tiene subarbol
+                pila.push(p); //se agrega el nodo actual a la pila
 
-                aux = this.gradoRegistro(p);
-                p = p.valor;
-                mayor = mayor >= aux ? mayor : aux;
+                aux = this.gradoRegistro(p); //se calcula el grado del subarbol del nodo actual
+                p = p.valor; //se mueve el puntero al subarbol
+                mayor = mayor >= aux ? mayor : aux; //se compara el grado mayor actual con el grado del subarbol del nodo actual y se guarda el mayor
             }
         }
-        return mayor
+        return mayor //se retorna el grado mayor
     };
 
-    arbol.prototype.numeroHojas = function () {
-        let p = this.raiz;
-        let pila = new Stack();
-        let padre = true;
-        let contador = 0;
-        while (!this.finDeRecorrido(p)) {
-            if (p.sw == 0) {
-                contador = !padre ? contador + 1 : contador;
-                padre = false;
-                p = p.getLigaDer();
+    arbol.prototype.numeroHojas = function () { //metodo que calcula el numero de hojas del arbol
+        let p = this.raiz; //se crea un puntero que apunta al nodo raiz
+        let pila = new Stack(); 
+        let padre = true; //variable que indica si el nodo actual es el padre de un subarbol
+        let contador = 0;   //contador que cuenta el numero de hojas del arbol
+        while (!this.finDeRecorrido(p)) { //mientras el puntero no apunte a un nodo nulo
+            if (p.sw == 0) { //si el nodo no tiene subarbol
+                contador = !padre ? contador + 1 : contador; //se suma 1 al contador si el nodo actual no es el padre de un subarbol
+                padre = false; //se indica que el nodo actual no es el padre de un subarbol
+                p = p.getLigaDer(); //se mueve el puntero al nodo derecho
                 while (!pila.empty() && p == null) {
                     p = pila.pop();
                     p = p.getLigaDer();
                 }
             }
-            else {
+            else { //si el nodo tiene subarbol, avanza normalmente al subarbol
                 pila.push(p);
                 p = p.valor;
-                padre = true;
+                padre = true; //se indica que el nodo actual es el padre de un subarbol
             }
         }
-        return contador;
+        return contador; //se retorna el numero de hojas del arbol
     };
-    arbol.prototype.altura = function(raiz){
-        let p = raiz;
-        if(this.finDeRecorrido(p))return 0;
-        if(this.finDeRecorrido(p.getLigaDer()))return 1;
-        let h=1,aux;
-        p = p.getLigaDer();
-        while(!this.finDeRecorrido(p)){
-            if(p.getSw()==1){
-                aux = this.altura(p.getValor());
-                if(aux>h){
-                    h=aux;
+    arbol.prototype.altura = function(raiz){ //metodo que calcula la altura del arbol
+        let p = raiz; //se crea un puntero que apunta al nodo raiz
+        if(this.finDeRecorrido(p))return 0; //si el puntero apunta a un nodo nulo, se retorna 0
+        if(this.finDeRecorrido(p.getLigaDer()))return 1; //si la liga derecha apunta a un nodo nulo, se retorna 1 pues no tiene hijos
+        let h=1,aux; //se crea una variable que guarda la altura del subarbol del nodo actual y una variable auxiliar
+        p = p.getLigaDer(); //se mueve el puntero al primer hijo del subarbol del nodo actual
+        while(!this.finDeRecorrido(p)){ //mientras el puntero no apunte a un nodo nulo
+            if(p.getSw()==1){ //si el nodo tiene subarbol
+                aux = this.altura(p.getValor()); //se calcula la altura del subarbol del nodo actual
+                if(aux>h){ //si la altura del subarbol del nodo actual es mayor que la altura actual
+                    h=aux; //se guarda la altura del subarbol del nodo actual
                 }
             }
-            p = p.ligaDer;
+            p = p.ligaDer; //se mueve el puntero al siguiente hijo del subarbol del nodo actual
         }
-        return h+1;
+        return h+1; //se retorna la altura del arbol
     }
 
-    arbol.prototype.gradoDato = function (dato){
-        let grado = -1;
+    arbol.prototype.gradoDato = function (dato){ //metodo que calcula el grado del dato en el arbol
+        let grado = -1; //se crea una variable que guarda el grado del dato, por defecto es -1(que significa que no existe)
+        //se recorre el arbol buscando el dato
         let p = this.raiz;
         let pila = new Stack();
         while (!this.finDeRecorrido(p)) {
             if (p.sw == 0) {
-                if (p.valor == dato) {
-                    return this.gradoRegistro(p);
+                if (p.valor == dato) { //si el dato es el mismo que el nodo actual
+                    return this.gradoRegistro(p); //se retorna el grado del nodo actual
                 }
                 p = p.getLigaDer();
                 while (!pila.empty() && p == null) {
@@ -235,33 +234,34 @@ var arbol = /** @class */ (function () {
                 p = p.valor;
             }
         }
-        alert("No se encontro el dato");
-        return grado;
+        alert("No se encontro el dato"); //si no se encuentra el dato, se muestra un mensaje de error
+        return grado; //se retorna el grado del dato
     }
-    arbol.prototype.nivelRegistro = function (registro){
-        if(registro==this.raiz){
-            return 1;
+    arbol.prototype.nivelRegistro = function (registro){ //metodo que calcula el nivel del registro en el arbol
+        if(registro==this.raiz){ //si el registro es el mismo que el nodo raiz
+            return 1; //se retorna 1
         }
-        if(this.finDeRecorrido(registro)){
-            return 0;
+        if(this.finDeRecorrido(registro)){ //si el registro es un nodo nulo
+            return 0; //se retorna 0
         }
-        let p = registro.getLigaIzq();
-        let i=1;
-        while(!this.finDeRecorrido(p)){
-            i = i+1;
-            p = p.getLigaIzq();
+        let p = registro.getLigaIzq(); //se crea un puntero que apunta al padre del registro
+        let i=1; //se crea una contador que cuenta el numero de padres del registro, es decir el nivel del registro
+        while(!this.finDeRecorrido(p)){ //mientras el puntero no apunte a un nodo nulo, es decir que tenga padre
+            i = i+1; //se suma 1 al contador
+            p = p.getLigaIzq(); //se mueve el puntero al padre del registro
         }
-        return i;
+        return i; //se retorna el nivel del registro
     }
 
-    arbol.prototype.nivelDato = function (dato){
-        let grado = -1;
+    arbol.prototype.nivelDato = function (dato){ //metodo que calcula el nivel del dato en el arbol
+        let grado = -1; //se crea una variable que guarda el grado del dato, por defecto es -1(que significa que no existe)
+        //se recorre el arbol buscando el dato
         let p = this.raiz;
         let pila = new Stack();
         while (!this.finDeRecorrido(p)) {
             if (p.sw == 0) {
-                if (p.valor == dato) {
-                    return this.nivelRegistro(p);
+                if (p.valor == dato) { //si el dato es el mismo que el nodo actual
+                    return this.nivelRegistro(p); //se retorna el nivel del nodo actual
                 }
                 p = p.getLigaDer();
                 while (!pila.empty() && p == null) {
@@ -275,31 +275,31 @@ var arbol = /** @class */ (function () {
             }
         }
 
-        return grado;
+        return grado; //se retorna el grado del dato
     }
 
-    arbol.prototype.ancestrosRegistro = function (registro){
-        let arregloPadres = [];
-        if(registro==this.raiz){
-            alert("El registro es la raiz, por lo tanto no tiene ancestros.");
-            return -1;
+    arbol.prototype.ancestrosRegistro = function (registro){ //metodo que calcula los ancestros del registro en el arbol
+        let arregloPadres = []; //se crea un arreglo que guarda los ancestros del registro
+        if(registro==this.raiz){ //si el registro es el mismo que el nodo raiz
+            alert("El registro es la raiz, por lo tanto no tiene ancestros."); //se muestra un mensaje de error
+            return -1; //se retorna -1
         }
-        let p=registro.getLigaIzq();
-        while(p!=null){
-            arregloPadres.push(p.valor);
-            p = p.getLigaIzq();
+        let p=registro.getLigaIzq(); //se crea un puntero que apunta al padre del registro
+        while(p!=null){ //mientras el puntero no apunte a un nodo nulo, es decir que tenga padre
+            arregloPadres.push(p.valor); //se agrega el padre del registro al arreglo
+            p = p.getLigaIzq(); //se mueve el puntero al padre del registro
         }
-        return arregloPadres;
+        return arregloPadres; //se retorna el arreglo con los ancestros del registro
     }
 
-    arbol.prototype.ancestrosDato = function (dato){
-        let arregloPadres = [];
-        let p = this.raiz;
+    arbol.prototype.ancestrosDato = function (dato){ //metodo que calcula los ancestros del dato en el arbol
+        //se recorre el arbol buscando el dato
+        let p = this.raiz; 
         let pila = new Stack();
         while (!this.finDeRecorrido(p)) {
             if (p.sw == 0) {
-                if (p.valor == dato) {
-                    return this.ancestrosRegistro(p);
+                if (p.valor == dato) { //si el dato es el mismo que el nodo actual
+                    return this.ancestrosRegistro(p); //se retorna el arreglo con los ancestros del nodo actual
                 }
                 p = p.getLigaDer();
                 while (!pila.empty() && p == null) {
